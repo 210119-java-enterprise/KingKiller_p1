381a22e8915afb826d69a53dc2e996686cf30189
Eric Newman <googlnewman@gmail.com>
2021-02-12 11:21:13 -0600

    Yikes its still super ugly but we have the first step of persistance down (can add a stringy employee to the db). NEED to spend time reworking structure of project. Also need to work on casting to proper types for SQL insertion.

--- KingKiller/src/main/java/com/revature/mapping/ObjectMapper.java
+++ KingKiller/src/main/java/com/revature/mapping/ObjectMapper.java
@@ -2,19 +2,24 @@ package com.revature.mapping;
 
 import com.revature.Mapper;
 import com.revature.util.ClassInspector;
+import com.revature.util.ConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ObjectMapper {
     private static Logger logger = LogManager.getLogger(Mapper.class);
-
+    private String SQLstatement;
     private Object userObj;
 
     public ObjectMapper(Object userObj) {
         this.userObj = userObj;
+        this.SQLstatement = null;
     }
 
     public ArrayList<String> getObjectValues(){
@@ -40,6 +45,7 @@ public class ObjectMapper {
         return objClassName;
     }
 
+    //REALLY crude way of gathering the input object values including class name and fields
     public ArrayList<String> getMapObj() {
         ArrayList<String> objValues = new ArrayList<>();
         String tableName = userObj.getClass().getName();
@@ -50,20 +56,23 @@ public class ObjectMapper {
         return objValues;
     }
 
-    public void mapObj() {
+    //sets up the prepared sql insert statement with mapped obj
+    public void setSqlInsert() {
         ArrayList<String> objList = getMapObj();
+        System.out.println("objlist: " + objList);
         System.out.println("objlist size: " + objList.size());
         String tableName = objList.get(0);
         ArrayList<String> tableColumns = ClassInspector.getFieldNames(userObj.getClass());
         System.out.println("table columns: " + tableColumns);
+        System.out.println("# table cols: " + tableColumns.size());
         //CHECK IF TABLENAME IS IN XML MAPS
         //GET PROPER DATA TYPES FROM XML FILES TO CHANGE STRINGS TOO
         String sqlStatement = null;
         StringBuilder insertInto = new StringBuilder("INSERT INTO " + tableName + " (");
         StringBuilder values = new StringBuilder("VALUES (");
-        for(int i = 0; i < objList.size() - 2; i++){
-            if(i == 0) {
-                insertInto.append(tableName).append(") ");
+        for(int i = 0; i < objList.size() - 1; i++){
+            if(i == (objList.size() - 2)) {
+                insertInto.append(tableColumns.get(i)).append(") ");
                 values.append(" ? ").append(") ");
             }else {
                 insertInto.append(tableColumns.get(i)).append(", ");
@@ -71,7 +80,35 @@ public class ObjectMapper {
             }
         }
         sqlStatement = insertInto.toString() + values.toString();
-        System.out.println(sqlStatement);
+        //System.out.println("Prepared SQL statement gathered: " + sqlStatement);
+        this.SQLstatement = sqlStatement;
     }
+
+    public void mapObj(){
+        ArrayList<String> valList = null;
+        setSqlInsert();
+        if (SQLstatement != null) {
+            valList = getObjectValues();
+        }
+        System.out.println("Prepared SQL statement gathered: " + this.SQLstatement);
+        System.out.println("Prepared values for SQL statement: " + valList);
+
+        try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+            System.out.println("DB Schema: " + conn.getSchema());
+            PreparedStatement pstmt = conn.prepareStatement(SQLstatement);
+
+            for(int i = 0; i < valList.size(); i++){
+                pstmt.setObject(i + 1, valList.get(i));
+            }
+            System.out.println(pstmt.toString());
+
+            pstmt.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
